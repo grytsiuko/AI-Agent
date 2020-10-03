9669108ae23e5c482b55e2d82fe025b45cf15bf5
Oleksandr Hrytsiuk <grytsiuk.o@gmail.com>
2020-10-03 20:14:54 +0300

    Memory measure

--- src/agent/Agent.java
+++ src/agent/Agent.java
@@ -3,13 +3,17 @@ package agent;
 public abstract class Agent {
 
     public void benchmark() {
+        Runtime runtime = Runtime.getRuntime();
+        runtime.gc();
         long start = System.nanoTime();
         run();
         long end = System.nanoTime();
         long delta = end - start;
+        long memory = runtime.totalMemory() - runtime.freeMemory();
 
         System.out.println("Spent time: " + (delta / 1e+6) + "ms");
         System.out.println("Total cost: " + getTotalCost());
+        System.out.println("Memory:     " + (memory / 1024 / 1024) + "Mb");
     }
 
     public abstract void run();
